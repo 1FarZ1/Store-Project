9fc92da6080d5546984094179b9e281d888bb194
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-10 09:04:54 +0100

    completing the simple front end

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const productRouter = require('./routes/products');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./public'));
+app.use(express.static('./view'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
