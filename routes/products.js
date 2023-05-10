559cdb2f0e8d7e3581d92f7f029d73fa5483d19c
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-10 08:55:53 +0100

    adding router

---
+++ routes/products.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const productRouter = express.Router();
+
+const { getAllProducts, getProductById } = require("../controllers/products")
+
+productRouter.route("/").get(getAllProducts)
+productRouter.route("/:id").get(getProductById)
+
+module.exports = productRouter;
